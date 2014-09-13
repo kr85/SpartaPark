f951318b1ee84e1a0733e6eae354d3fc2658796e
Kosta Rashev <ni6to.bg@gmail.com>
2014-09-12 21:01:04 -0700

    Add lot repository interface

---
+++ app/SpartaPark/Repository/Lot/LotRepository.php
@@ -0,0 +1,14 @@
+<?php namespace SpartaPark\Repository\Lot;
+
+use SpartaPark\Repository\Crudable;
+use SpartaPark\Repository\Repository;
+
+/**
+ * Interface LotRepository
+ * 
+ * @package SpartaPark\Repository\Lot
+ */
+interface LotRepository extends Repository, Crudable
+{
+
+}
