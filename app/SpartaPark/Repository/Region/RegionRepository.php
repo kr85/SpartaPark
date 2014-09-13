070322a34355ae8d8d6f3d57754096d78fc26dfa
Kosta Rashev <ni6to.bg@gmail.com>
2014-09-12 21:18:48 -0700

    Add region repository interface

---
+++ app/SpartaPark/Repository/Region/RegionRepository.php
@@ -0,0 +1,14 @@
+<?php namespace SpartaPark\Repository\Region;
+
+use SpartaPark\Repository\Crudable;
+use SpartaPark\Repository\Repository;
+
+/**
+ * Interface RegionRepository
+ * 
+ * @package SpartaPark\Repository\Region
+ */
+interface RegionRepository extends Repository, Crudable
+{
+
+}
