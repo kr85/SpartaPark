4d7adf38f0d4ef0162e8186e8d94b5ee6ff25ac3
Kosta Rashev <ni6to.bg@gmail.com>
2014-09-12 20:44:55 -0700

    Add owner repository interface

---
+++ app/SpartaPark/Repository/Owner/OwnerRepository.php
@@ -0,0 +1,14 @@
+<?php namespace SpartaPark\Repository\Owner;
+
+use SpartaPark\Repository\Crudable;
+use SpartaPark\Repository\Repository;
+
+/**
+ * Interface OwnerRepository
+ *
+ * @package SpartaPark\Repository\Owner
+ */
+interface OwnerRepository extends Repository, Crudable
+{
+
+}
