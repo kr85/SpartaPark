83f2b6e11604c5817e4851d6a4d920157897d1fa
Kosta Rashev <ni6to.bg@gmail.com>
2014-10-05 15:37:44 -0700

    Add contact form css

--- app/views/contacts/index.blade.php
+++ app/views/contacts/index.blade.php
@@ -1,53 +1,76 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container">
-        <div class="content-wrapper content-wrapper-navbar-push">
+    <div class="content-wrapper-navbar-push">
+        <div class="content-wrapper">
             <h3>Questions. Concerns. Feedback. No Problem!</h3>
-            <h3>Drop Us A Line</h3>
 
-            {{ Form::open(array('route' => 'contact.request')) }}
+            <div class="contact-form-wrapper">
+                <div class="contact-form-center">
+                    <h3>Drop Us A Line</h3>
 
-                @if(count($errors->all()))
-                    <ul class="errors">
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                @endif
+                    {{ Form::open(array('route' => 'contact.request')) }}
 
-                <div class="row">
-                    <div class="col-md-2">
-                        {{ Form::label('first_name', 'First Name:') }}
+                        @if(count($errors->all()))
+                            <ul class="errors">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                {{ Form::label('first_name', 'First Name:') }}
+                                <br />
+                                {{ Form::text('first_name', '', array('class' => 'col-xs-12 col-sm-12 col-md-12')) }}
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                {{ Form::label('last_name', 'Last Name:') }}
+                                <br />
+                                {{ Form::text('last_name', '', array('class' => 'col-xs-12 col-sm-12 col-md-12')) }}
+                            </div>
+                        </div>
                         <br />
-                        {{ Form::text('first_name', '') }}
-                    </div>
-                    <div class="col-md-2">
-                        {{ Form::label('last_name', 'Last Name:') }}
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                {{ Form::label('email', 'E-mail Address:') }}
+                                <br />
+                                {{ Form::email('email', '', array('placeholder' => 'example@email.com', 'class' => 'textarea-style')) }}
+                            </div>
+                        </div>
                         <br />
-                        {{ Form::text('last_name', '') }}
-                    </div>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                {{ Form::label('subject', 'Subject:') }}
+                                <br />
+                                {{ Form::text('subject', '', array('class' => 'textarea-style')) }}
+                            </div>
+                        </div>
+                        <br />
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                {{ Form::label('message', 'Message:') }}
+                                <br />
+                                {{ Form::textarea('message', '', array('class' => 'textarea-style')) }}
+                            </div>
+                        </div>
+                        <br />
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="pull-right">
+                                    {{ Form::reset('Reset', array('class' => 'btn btn-default')) }}
+                                    {{ Form::submit('Send', array('class' => 'btn btn-primary')) }}
+                                </div>
+                            </div>
+                        </div>
+                    {{ Form::close() }}
+                    <br /><br />
                 </div>
-                <br />
-                {{ Form::label('email', 'E-mail Address:') }}
-                <br />
-                {{ Form::email('email', '', array('placeholder' => 'example@email.com')) }}
-                <br /><br />
-
-                {{ Form::label('subject', 'Subject:') }}
-                <br />
-                {{ Form::text('subject', '') }}
-                <br /><br />
-
-                {{ Form::label('message', 'Message:') }}
-                <br />
-                {{ Form::textarea('message', '') }}
-                <br /><br />
-
-                {{ Form::reset('Reset', array('class' => 'btn btn-default')) }}
-                {{ Form::submit('Send', array('class' => 'btn btn-primary')) }}
-            {{ Form::close() }}
-            <br /><br /><br /><br />
+            </div>
         </div>
     </div>
 @stop
