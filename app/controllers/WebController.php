518a0651cdc1a9e8748579d0c117a209fd6e92f8
Kosta Rashev <ni6to.bg@gmail.com>
2014-10-09 17:18:03 -0700

    Update web controller and main css

--- app/controllers/WebController.php
+++ app/controllers/WebController.php
@@ -154,6 +154,37 @@ class WebController extends BaseController
     */
    public function getAvailableParking()
    {
+      /*$config['center'] = '37.335, -121.880';
+      $config['zoom'] = 'auto';
+      Gmaps::initialize($config);
+
+      $marker = array();
+      $marker['position'] = '37.3353235, -121.8804712';
+      $marker['infowindow_content'] = 'San Jose State University';
+      Gmaps::add_marker($marker);
+
+      $marker = array();
+      $marker['position'] = '37.33225930, -121.88335920';
+      $marker['infowindow_content'] = 'SJSU West Parking Garage';
+      $marker['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=P|0099FF|000000';
+      Gmaps::add_marker($marker);
+
+      $marker = array();
+      $marker['position'] = '37.33347370, -121.87991640';
+      $marker['infowindow_content'] = 'SJSU South Parking Garage';
+      $marker['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=P|0099FF|000000';
+      Gmaps::add_marker($marker);
+
+      $marker = array();
+      $marker['position'] = '37.33847300, -121.88054690';
+      $marker['infowindow_content'] = 'SJSU North Parking Garage';
+      $marker['icon'] = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=P|0099FF|000000';
+      Gmaps::add_marker($marker);
+
+      $data['map'] = Gmaps::create_map();
+
+      return $this->layout = View::make('spartapark.availableparking', $data);*/
+
       return $this->layout = View::make('spartapark.availableparking');
    }
 
--- app/views/spartapark/availableparking.blade.php
+++ app/views/spartapark/availableparking.blade.php
@@ -1,11 +1,24 @@
 @extends('layouts.master')
 
+@section('script')
+
+@stop
+
 @section('content')
-    <div class="container">
-        <div class="content-wrapper content-wrapper-navbar-push">
-            <h3>Available Parking Here Soon...</h3>
-        </div>
+    <div class="content-wrapper content-wrapper-navbar-push">
     </div>
+
+
+
+    <!--<div class="">
+        <div class="content-wrapper content-wrapper-navbar-push">
+                <div class="col-md-9">
+                    <div id="map_canvas">
+
+                    </div>
+                </div>
+        </div>
+    </div>-->
 @stop
 
 @section('footer-assets')
