348289ec50fdb68784fea039046987c6a070d5a2
Kosta Rashev <ni6to.bg@gmail.com>
2014-10-12 03:26:17 -0700

    Bind Google Maps API markers to sidebar

--- app/views/spartapark/availableparking.blade.php
+++ app/views/spartapark/availableparking.blade.php
@@ -2,6 +2,7 @@
 
 @section('script')
     <script>
+        // SJSU information
         var owner = {
             'name': 'San Jose State University',
             'address': '1 Washington Square, San Jose, CA 95192',
@@ -10,8 +11,11 @@
             'longitude': -121.8804712
         };
 
+        // All lots
         var lots = <?php echo json_encode($availableParking); ?>;
+        // Map
         var map;
+        // Info window
         var infoWindow = new google.maps.InfoWindow;
 
         // Initialize the map
@@ -26,12 +30,15 @@
 
             map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 
+            // Initialize each lod
             for (id in lots) {
                 initializePoint(lots[id]);
             }
 
+            // Add SJSU marker
             var sjsuMarker = placeMarker(myLatLng);
 
+            // Creates maker's info window content
             var html = '<strong style="font-size: 15px; line-height: 1.5; margin-bottom: 10px;">' + owner.name +
                   '</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />' +
                   '<hr style="margin-top: 10px; margin-bottom: 10px;">';
@@ -43,7 +50,11 @@
                 '<a href="http://www.sjsu.edu/">Web Site</a><hr style="margin-top: 8px; margin-bottom: 8px;">' +
                 '<a href="http://en.wikipedia.org/wiki/San_Jose_State_University" style="font-size: 13px; float: right;">' +
                 '<strong>See Details</strong></a>';
+
+            // Binds the info window to the marker for mouseover
             bindInfoWindow(sjsuMarker, map, infoWindow, html);
+
+            // Binds the info window to the marker for mouse click
             bindInfoWindowOnClick(sjsuMarker, map, infoWindow, html);
         }
 
@@ -87,7 +98,7 @@
                 'vertical-align: middle; text-align: center; padding-bottom: 14px;"><strong>' +
                 lotData.spots_available + ' Available Parking ' + spots + '</strong></div>';
 
-            bindInfoWindow(marker, map, infoWindow, html);
+            bindInfoWindow(marker, map, infoWindow, html, lotData.id);
 
             html = html + '<hr style="margin-top: 6px;">';
 
@@ -113,39 +124,55 @@
 
             regionTable = regionTable + regionTableBody + '</table>';
 
-            html = html + regionTable + '<hr><a href="#" style="float: right; padding-bottom: 10px; font-size: 13px;"><strong>Directions</strong></a>';
+            html = html + regionTable + '<hr><a href="#" style="float: right; padding-bottom: 10px; font-size: 13px;">' +
+                '<strong>Directions</strong></a>';
 
-            bindInfoWindowOnClick(marker, map, infoWindow, html);
+            bindInfoWindowOnClick(marker, map, infoWindow, html, lotData.id);
+
+            createLotEntry(marker, lotData);
 
             marker.setMap(map);
         }
 
         // Binds info window on hover
-        function bindInfoWindow(marker, map, infoWindow, html)
+        function bindInfoWindow(marker, map, infoWindow, html, id)
         {
+            var id = "#lot-" + id;
+
             google.maps.event.addListener(marker, 'mouseover', function() {
                 infoWindow.setContent(html);
                 infoWindow.open(map, marker);
+                $("div").removeClass("lot-entry-hover");
+                $(id).addClass("lot-entry-hover");
             });
 
             google.maps.event.addListener(marker, 'mouseout', function() {
                 infoWindow.close();
+                $(id).removeClass("lot-entry-hover");
             });
         }
 
         // Binds info window on click
-        function bindInfoWindowOnClick(marker, map, infoWindow, html)
+        function bindInfoWindowOnClick(marker, map, infoWindow, html, id)
         {
+            var id = "#lot-" + id;
+
             google.maps.event.addListener(marker, 'click', function() {
                 google.maps.event.clearListeners(marker, 'mouseout');
                 infoWindow.setContent(html);
                 infoWindow.open(map, marker);
+                $("div").removeClass("lot-entry-active");
+                $("div").removeClass("lot-entry-hover");
+                $(id).addClass("lot-entry-active");
             });
 
             google.maps.event.addListener(map, 'click', function() {
                 infoWindow.close();
+                $("div").removeClass("lot-entry-active");
+                $("div").removeClass("lot-entry-hover");
                 google.maps.event.addListener(marker, 'mouseout', function() {
                     infoWindow.close();
+                    $(id).removeClass("lot-entry-hover");
                 });
             });
         }
@@ -162,32 +189,62 @@
             return marker;
         }
 
+        function createLotEntry(marker, lotData)
+        {
+            var ul = document.getElementById("marker_list");
+            var li = document.createElement("li");
+
+            var distance = Math.round(lotData.distance * 100) / 100;
+
+            var html = '<div class="lot-entry" id="lot-' + lotData.id + '"><div class="lot-entry-container"><div class="lot-entry-name" id="lot-name">' + lotData.name +
+                  '</div>' +
+                  '<div class="pull-right" id="lot-distance">' + distance + ' miles</div><br />';
+
+            var address = lotData.address;
+            var addressLine1 = address.split(",")[0];
+            var addressLine2 = address.split(",")[1] + ", " + address.split(",")[2];
+
+            if (lotData.spots_available == 0) {
+                var spots = "spots";
+                var color = "red"
+            }
+            else if (lotData.spots_available == 1) {
+                var spots = "Spot";
+                var color = "green"
+            } else {
+                var spots = "Spots"
+                var color = "green";
+            }
+
+            html = html + addressLine1 + '<br />' + addressLine2;
+
+            li.innerHTML = html;
+            ul.appendChild(li);
+
+            google.maps.event.addDomListener(li, "mouseover", function(){
+                google.maps.event.trigger(marker, "mouseover");
+            });
+
+            google.maps.event.addDomListener(li, "mouseout", function(){
+                google.maps.event.trigger(marker, "mouseout");
+            });
+
+            google.maps.event.addDomListener(li, "click", function(){
+                google.maps.event.trigger(marker, "click");
+            });
+        }
+
         google.maps.event.addDomListener(window, 'load', initialize);
     </script>
 @stop
 
 @section('content')
     <div class="content-wrapper">
-        <div class="row">
-            <div class="col-md-2">
-                <?php
-                    if ($availableParking)
-                    {
-                        foreach ($availableParking as $lot) {
-                            echo '<div class="lot-entry">';
-                            echo '<div class="lot-entry-container">';
-                            echo '<div class="lot-entry-name">' . $lot['name'] . '</div>';
-                            $address = $lot['address'];
-                            $address = explode(",", $address);
-                            echo '<div>' . $address[0] . '<br />';
-                            echo $address[1] . ', ' . $address[2] . '</div>';
-                            echo '</div>';
-                            echo '</div>';
-                        }
-                    }
-                ?>
+        <div class="sidebar-map-area-wrapper">
+            <div class="sidebar">
+                <ul id="marker_list"></ul>
             </div>
-            <div class="col-md-10">
+            <div class="map-area">
                 <div class="map-canvas" id="googleMap"></div>
             </div>
         </div>
