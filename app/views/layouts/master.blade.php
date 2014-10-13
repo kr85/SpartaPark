c158f8688ae13492eb44e6868c36cec78c8a19da
Kosta Rashev <ni6to.bg@gmail.com>
2014-10-13 01:32:25 -0700

    Fix X-UA-Compatible validation issue

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,11 +1,14 @@
+<!--[if HTML5]><![endif]-->
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
+        <!--[if !HTML5]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="shortcut icon" href="<?php echo URL::to('assets/images/favicon.ico'); ?>">
         <title>SpartaPark  | SJSU Parking Guidance System</title>
         <meta name="description" content="A Parking Guidance System">
